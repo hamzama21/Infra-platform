0677e4c419dacc872f27b540f0f762a8e0feaa7d
hamzama21 <hamzalahrour5@gmail.com>
2026-09-12 16:32:17 +0200

    Ajout VPC, subnet, security group, key pair et instance EC2 k3s

---
+++ terraform/main.tf
@@ -0,0 +1,137 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "idp-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block               = "10.0.1.0/24"
+  availability_zone        = "eu-west-3a"
+  map_public_ip_on_launch  = true
+
+  tags = {
+    Name = "idp-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "idp-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "idp-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "k3s" {
+  name        = "idp-k3s-sg"
+  description = "Security group for k3s node"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "idp-k3s-sg"
+  }
+}
+
+resource "aws_key_pair" "idp" {
+  key_name   = "idp-key"
+  public_key = file(pathexpand("~/.ssh/idp-key.pub"))
+
+  tags = {
+    Name = "idp-key"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd*/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "k3s" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.k3s.id]
+  key_name               = aws_key_pair.idp.key_name
+
+  user_data = <<-EOF
+    #!/bin/bash
+    curl -sfL https://get.k3s.io | sh -
+  EOF
+
+  tags = {
+    Name = "idp-k3s-node"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.k3s.public_ip
+}
